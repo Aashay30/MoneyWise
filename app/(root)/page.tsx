74986798120978dbb327362904178e25b9acd532
Aashay30 <tamrakaraashay30@gmail.com>
2024-08-31 02:53:27 +0530

    added header box component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,25 @@
 // to have page routing i.e ROUTE GROUPS we have created the foder with parenthesis
 
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+
+    const loggedIn = { firstName: 'Aashay' };
+
   return (
-    <div>
-      HOME
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+            <HeaderBox
+                type = "greeting"
+                title = "Welcome"
+                user = {loggedIn?.firstName || 'Guest'}
+                subtext = "Access and manage your account and transactions efficiently."
+            />
+        </header>
+      </div>
+    </section>
   )
 }
 
