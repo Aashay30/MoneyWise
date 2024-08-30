8174b4455692c66b3e737016d6bfb5fbbd531e02
Aashay30 <tamrakaraashay30@gmail.com>
2024-08-31 02:23:44 +0530

    implementing route groups

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+// to have page routing i.e ROUTE GROUPS we have created the foder with parenthesis
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
